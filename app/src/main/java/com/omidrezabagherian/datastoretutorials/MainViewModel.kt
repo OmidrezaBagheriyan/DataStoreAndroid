9886a3203f52838777d3b73ce730656ff599a2a9
OmidrezaBagheriyan <OmidCoder21@gmail.com>
2023-07-01 19:24:59 +0330

    Fix error MainViewModel.kt

--- app/src/main/java/com/omidrezabagherian/datastoretutorials/MainViewModel.kt
+++ app/src/main/java/com/omidrezabagherian/datastoretutorials/MainViewModel.kt
@@ -1,13 +1,14 @@
 package com.omidrezabagherian.datastoretutorials
 
 import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class MainViewModel(application: Application) : ViewModel() {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
     private val dataStore = SettingDataStore(application)
 
     val getTheme = dataStore.getTheme().asLiveData(Dispatchers.IO)
